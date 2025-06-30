f636a32de11f3ecab49ff1cc82e90303bcde7a66
Admin <Admin@LAPTOP-9VBUI8U7>
2025-06-30 15:19:05 +0530

    day 2

---
+++ Training_Day2/src/com/tns/day2/Loops.java
@@ -0,0 +1,48 @@
+package com.tns.day2;
+
+public class Loops {
+	public static void main(String[] args) {
+		/* 
+		 * Increment operation :
+		 * pre-increment (++a)
+		 * post increment(a++) 
+		  */
+		/*int a=10;
+		
+		int c=(++a)+(a++)+(a++)+(++a)-(a);
+		System.out.println(c);*/
+		// Output: 
+		
+		/*
+		 * int a=-1; int b=1; int c=0; int
+		 * result=(--c)+(++b)-(a++)-(++b)+(b++)-(a)+(--a)+(--c)+(c++)+(--c)-(++b)+(c)+(b
+		 * --); System.out.println(result);
+		 */
+		// Output: -7
+		
+		
+		
+		
+		
+		// IF statement 
+		// IfElse statement 
+		
+		/*
+		 * int a=10;
+		 * 
+		 * if (a!=0) { System.out.println("Value is grater than 0"); } else {
+		 * System.out.println("value is less than 0"); }
+		 */
+		//Output: Value is grater than 0
+		
+		
+		// Nested loop 
+		
+		int a=10; int b=20; int c=50;
+		
+		if 
+		
+		
+}
+	
+}
