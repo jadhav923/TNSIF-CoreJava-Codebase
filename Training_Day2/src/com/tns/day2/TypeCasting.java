83cd355925e63436ff006f6aa442b1627bca61bf
Admin <Admin@LAPTOP-9VBUI8U7>
2025-06-30 14:09:02 +0530

    day 2

---
+++ Training_Day2/src/com/tns/day2/TypeCasting.java
@@ -0,0 +1,11 @@
+package com.tns.day2;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hii");
+
+	}
+
+}
--- Training_Day2/src/com/tns/day2/practicce1.java
+++ Training_Day2/src/com/tns/day2/practicce1.java
@@ -3,12 +3,32 @@ package com.tns.day2;
 public class practicce1 {
 
 	public static void main(String[] args) {
-		System.out.println("Variable program");
-		int a=10;
-		int b=20;
-		int c=a+b;
-		System.out.println("A:"+a +"  B:"+b);
-		System.out.println("Addition of Val A & Val B is"+ c);
+		/*
+		 * System.out.println("Variable program"); int a=10; 
+		 * int b=20; int c=a+b;
+		 * System.out.println("A:"+a +"  B:"+b);
+		 * System.out.println("Addition of Val A & Val B is"+ c);
+		 */
+		
+		/*
+		 * int val=10/9; 
+		 * System.out.println(val);
+		 * 
+		 * float val2=10f/9f; 
+		 * System.out.println(val2);
+		 * 
+		 * double val3=10d/9d; 
+		 * System.out.println(val3);
+		 * 
+		 * char ch=66; 
+		 * System.out.println(ch);
+		 * 
+		 * char ch2='\u0061'; System.out.println(ch2);
+		 */
+		
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println("Min range of byte is:"+ byteMax + "Max range of byte is: "+ byteMin);
 		
 		
 
