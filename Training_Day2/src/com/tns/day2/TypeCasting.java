c32551089a1fef809428db756254137943cf292b
Admin <Admin@LAPTOP-9VBUI8U7>
2025-06-30 15:18:48 +0530

    day 2

--- Training_Day2/src/com/tns/day2/TypeCasting.java
+++ Training_Day2/src/com/tns/day2/TypeCasting.java
@@ -3,8 +3,14 @@ package com.tns.day2;
 public class TypeCasting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hii");
+		int a=10;
+		float b=(float) a;
+		System.out.println(b);
+		
+		long phone_no=999999998;
+		int n=(int) phone_no;
+		System.out.println(n);
+		
 
 	}
 
