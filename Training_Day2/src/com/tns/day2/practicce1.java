ab4e61869d9d63da1a76cfa0b93c8f0d6e2771de
Admin <Admin@LAPTOP-9VBUI8U7>
2025-06-30 12:00:13 +0530

    day 2

--- Training_Day2/src/com/tns/day2/practicce1.java
+++ Training_Day2/src/com/tns/day2/practicce1.java
@@ -3,7 +3,14 @@ package com.tns.day2;
 public class practicce1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("Variable program");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("A:"+a +"  B:"+b);
+		System.out.println("Addition of Val A & Val B is"+ c);
+		
+		
 
 	}
 
